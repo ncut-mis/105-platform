aaf7151bf482667fe78c364bf213fcb24e762097
3A532059 <coco214034@gmail.com>
2019-05-21 18:14:06 +0800

    token 隱藏

--- resources/views/modi.blade.php
+++ resources/views/modi.blade.php
@@ -48,29 +48,30 @@
                         <div>
                             <input name="name" class="form-control" placeholder="請輸入姓名" value="{{$member->name}}" required>
                         </div>
-                        <p class="mbr-author-desc mbr-fonts-style display-7">
-                            <font face="微軟正黑體">個人驗證碼：</font>
-                        </p>
-                        <div>
-                            <input name="token" class="form-control" placeholder="請輸入下方顯示之個人驗證碼" value="{{$member->token}}" required>
-                        </div>
-                        <p id="token"></p>
+
+                        <p id="token" hidden></p>
                         <script>
-                            const messaging = firebase.messaging();
-                            messaging.requestPermission().then(res=>
-                                {
-                                    return messaging.getToken();
-                                }
-                                , err => {
-                                    // 若拒絕通知
-                                    console.log(err);
-                                })
-                                .then(token => {
-                                    // 成功取得 token
-                                    console.log(token);
-                                    document.getElementById("token").innerHTML = "目前個人驗證碼為：<br>" +token+"<br>為使各餐廳之優惠卷能成功發送您的手上，請將上述代碼複製並貼上於上方框框中，謝謝。";
-                                });
+
+                                const messaging = firebase.messaging();
+                                messaging.requestPermission().then(res=>
+                                    {
+                                        return messaging.getToken();
+                                    }
+                                    , err => {
+                                        // 若拒絕通知
+                                        console.log(err);
+                                    })
+                                    .then(token => {
+                                        // 成功取得 token
+                                        console.log(token);
+                                        document.getElementById("token").innerHTML = "目前個人驗證碼為：<br>" +token+"<br>為使各餐廳之優惠卷能成功發送您的手上，請將上述代碼複製並貼上於上方框框中，謝謝。";
+                                        document.getElementById("abc").value=token;
+                                    });
                         </script>
+                        <div>
+                            <input name="token" class="form-control" placeholder="請輸入下方顯示之個人驗證碼" id="abc" required disabled hidden>
+                        </div>
+
                         <div class="col-md-12" style="text-align:center">
                             <div class="text-center">
                                 <button type="submit" class="btn btn-success"><span class="mbri-update"></span><font face="微軟正黑體">更新</font></button>
